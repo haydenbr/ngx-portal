c672ca2feaf3db71be3fc86aa6c20d58c50ab974
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2020-01-28 19:19:53 -0500

    woops. fixed it

--- projects/ngx-portal/src/lib/ngx-portal.module.ts
+++ projects/ngx-portal/src/lib/ngx-portal.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common'
 
 import { NgxPortalRouterService } from './ngx-portal-router.service'
+import { NgxPortalInputComponent } from './ngx-portal-input.component'
+import { NgxPortalOutputComponent } from './ngx-portal-output.component'
 
 @NgModule({
-	exports: [],
-	declarations: [],
+	imports: [CommonModule],
+	exports: [
+		NgxPortalInputComponent,
+		NgxPortalOutputComponent
+	],
+	declarations: [
+		NgxPortalInputComponent,
+		NgxPortalOutputComponent
+	],
 	providers: [NgxPortalRouterService],
 })
 export class NgxPortalModule { }
