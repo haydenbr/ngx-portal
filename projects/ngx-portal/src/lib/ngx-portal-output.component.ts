e70481dab79c880fbd93b94e51b0256b086a952b
Hayden Braxton <haydenbraxton@unboxedtechnology.com>
2020-05-05 19:28:25 -0400

    fix: trying again to prevent the change after checked error

--- projects/ngx-portal/src/lib/ngx-portal-output.component.ts
+++ projects/ngx-portal/src/lib/ngx-portal-output.component.ts
@@ -1,20 +1,29 @@
 import { Component, ChangeDetectionStrategy, TemplateRef, Input, OnInit } from '@angular/core';
 import { Observable } from 'rxjs'
+import { debounceTime } from 'rxjs/operators'
 
 import { NgxPortalRouterService } from './ngx-portal-router.service'
 
 @Component({
   selector: 'ngx-portal-output',
   template: `
-    <ng-container
-      *ngTemplateOutlet="portalContents$ | async"
-    ></ng-container>
+    <ng-container *ngIf="portalContents$">
+      <ng-container
+        *ngTemplateOutlet="portalContents$ | async"
+      ></ng-container>
+    </ng-container>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class NgxPortalOutputComponent {
+export class NgxPortalOutputComponent implements OnInit {
   @Input() portalKey: string;
-  portalContents$ = this.portals.getPortal(this.portalKey)
+  portalContents$: Observable<TemplateRef<any>>
 
   constructor(private portals: NgxPortalRouterService) {}
+
+  ngOnInit() {
+    this.portalContents$ = this.portals
+      .getPortal(this.portalKey)
+      .pipe(debounceTime(0))
+  }
 }
